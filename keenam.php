f54c979cde4371249b12e7b42efbfb99cbd8b2a1
asteriamelisa <145691342+asteriamelisa@users.noreply.github.com>
2024-09-06 15:46:27 +0700

    Praktikum 5D - Menggunakan Require Once (1)

--- keenam.php
+++ keenam.php
@@ -1,6 +1,5 @@
 <?php
-include 'kelima.php';
-include 'kelima.php';
-include 'kelima.php';
-include 'kelima.php';
+include_once 'kelima.php';
+include_once 'kelima.php';
+include_once 'ketujuh.php';
 echo "Ini file keenam.php\n";
