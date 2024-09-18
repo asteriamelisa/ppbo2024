ffe98b70f16452db31589661086e3167e81a4352
asteriamelisa <145691342+asteriamelisa@users.noreply.github.com>
2024-09-18 22:53:38 +0700

    Praktikum 6E - Menggunakan Pewarisan

---
+++ App/Admin/TenagaKependidikan.php
@@ -0,0 +1,11 @@
+<?php
+namespace TenagaKependidikan;
+
+
+class TenagaKependidikan
+{
+    public int $gaji_pokok;
+    public function cuti() : void {
+        echo "sedang mengambil cuti";
+    }
+}
