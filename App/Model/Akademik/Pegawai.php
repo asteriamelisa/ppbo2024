0db495681c78a34e40265ab233fe41e87aa33908
asteriamelisa <145691342+asteriamelisa@users.noreply.github.com>
2024-09-23 21:52:39 +0700

    Praktikum 6Dx - Implementasi Enkapsulasi dan Inheritance

--- App/Model/Akademik/Pegawai.php
+++ App/Model/Akademik/Pegawai.php
@@ -1,20 +1,25 @@
 <?php
 namespace App\Model\Akademik;
+
 class Pegawai {
     public $nip;
     public $nama;
     protected $no_hp;
     public $alamat;
+
     public function cekIn() : bool {
         return true;
     }
+
     public function cekOut() : bool {
         return true;
     }
-    public function getNoHp() : int {
+
+    // Ubah visibilitas dari public menjadi protected
+    protected function getNoHp() : int {
         return $this->no_hp;
     }
-    
+
     public function setNoHp(int $newNoHp) : void {
         $this->no_hp = $newNoHp;
     }
