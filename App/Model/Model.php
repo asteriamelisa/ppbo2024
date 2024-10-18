09c784e0a884813bbfbcbff3def3a7177ad95219
asteriamelisa <145691342+asteriamelisa@users.noreply.github.com>
2024-10-18 13:52:39 +0700

    Praktikum 10A - Membuat Koneksi dengan PDO

---
+++ App/Model/Model.php
@@ -0,0 +1,43 @@
+
+<?php
+
+
+namespace App\Model;
+
+
+header('Content-Type: application/json; charset=utf-8');
+
+
+use PDO;
+use PDOException;
+
+
+class Model
+{
+    protected $db;
+
+
+    public function __construct()
+    {
+        try {
+            $conn = new PDO("mysql:host=localhost;dbname=ppbo2024", 'root', '');
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->db = $conn;
+        } catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
+
+
+
+
+    }
+
+
+    public function getDB()
+    {
+        return $this->db;
+    }
+
+
+}
+
---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+
+$database = 'ppbo2024';
+$username = 'root';
+$password = '';
+$servername = 'localhost';
